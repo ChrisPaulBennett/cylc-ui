cd45ab01e964197f6af12628820828d2df4f4507
christopher.bennett <christopher.bennett@metoffice.gov.uk>
2025-03-04 10:49:35 +0000

    Fixed Chart Labels

--- src/components/cylc/analysis/BoxPlot.vue
+++ src/components/cylc/analysis/BoxPlot.vue
@@ -42,109 +42,131 @@ export default {
     },
     workflowName: {
       type: String,
-      required: true
+      default: null,
     },
-    configOptions: {
-      type: Object,
-      required: true
-    }
   },
-  components: {
-    apexchart: VueApexCharts
+
+  setup (props, { emit }) {
+    /**
+     * The 'sort by' state.
+     * @type {import('vue').Ref<string>}
+     */
+    const sortBy = useInitialOptions('sortBy', { props, emit }, 'name')
+
+    /**
+     * The page number state.
+     * @type {import('vue').Ref<number>}
+     */
+    const page = useInitialOptions('page', { props, emit }, 1)
+
+    /**
+     * The sort descending/sscending state.
+     * @type {import('vue').Ref<boolean>}
+     */
+    const sortDesc = useInitialOptions('sortDesc', { props, emit }, false)
+
+    const reducedAnimation = useReducedAnimation()
+
+    const chartOptions = computed(() => ({
+      chart: {
+        defaultLocale: 'en',
+        locales: [
+          {
+            name: 'en',
+            options: {
+              toolbar: {
+                exportToSVG: 'Download SVG',
+                exportToPNG: 'Download PNG',
+                menu: 'Download'
+              }
+            }
+          }
+        ],
+        animations: {
+          enabled: reducedAnimation.value ? false : props.animate,
+          easing: 'easeinout',
+          speed: 300,
+          animateGradually: {
+            enabled: true,
+            delay: 150,
+          },
+          dynamicAnimation: {
+            enabled: true,
+            speed: 350,
+          },
+        },
+        fontFamily: 'inherit',
+        toolbar: {
+          tools: {
+            download: `<svg class="w-100 h-100"><path d="${mdiDownload}"></path></svg>`,
+          },
+        },
+      },
+      tooltip: {
+        custom ({ seriesIndex, dataPointIndex, w }) {
+          const max = formatDuration(w.globals.seriesCandleC[seriesIndex][dataPointIndex], true, props.timingOption)
+          const q3 = formatDuration(w.globals.seriesCandleL[seriesIndex][dataPointIndex], true, props.timingOption)
+          const med = formatDuration(w.globals.seriesCandleM[seriesIndex][dataPointIndex], true, props.timingOption)
+          const q1 = formatDuration(w.globals.seriesCandleH[seriesIndex][dataPointIndex], true, props.timingOption)
+          const min = formatDuration(w.globals.seriesCandleO[seriesIndex][dataPointIndex], true, props.timingOption)
+          return `
+            <div class="pa-2">
+              <div>Maximum: ${max}</div>
+              <div>Q3: ${q3} </div>
+              <div>Median: ${med}</div>
+              <div>Q1: ${q1}</div>
+              <div>Minimum: ${min}</div>
+            </div>
+          `
+        },
+      },
+      plotOptions: {
+        bar: {
+          horizontal: true,
+        },
+        boxPlot: {
+          colors: {
+            upper: '#6DD5C2',
+            lower: '#6AA4F1',
+          },
+        },
+      },
+      xaxis: {
+        title: {
+          text: `${formatChartLabels(props.timingOption)}`,
+        },
+        labels: {
+          formatter: (value) => formatDuration(value, true, props.timingOption)
+        },
+      },
+    }))
+
+    return {
+      sortBy,
+      page,
+      sortDesc,
+      chartOptions,
+    }
   },
 
   computed: {
+    series () {
+      const sortedTasks = [...this.tasks].sort(this.compare)
+      const startIndex = Math.max(0, this.itemsPerPage * (this.page - 1))
+      const endIndex = Math.min(sortedTasks.length, startIndex + this.itemsPerPage)
 
-    boxPlot () {
-      const startingNumber = (this.configOptions.itemsPerPage * this.configOptions.page) - (this.configOptions.itemsPerPage)
-      let sortedTasks = []
-      let endingNumber = startingNumber + (this.configOptions.itemsPerPage)
-
-      sortedTasks = this.tasks
-      sortedTasks.sort(this.compare)
-
-      if (sortedTasks.length < this.configOptions.itemsPerPage) {
-        endingNumber = sortedTasks.length
-      }
-      const boxPlot = [{
-        data: []
-      }]
-
-      if (this.timingOption === 'totalTimes') {
-        for (let i = startingNumber; i < endingNumber; i++) {
-          boxPlot[0].data.push(
-            {
-              x: sortedTasks[i].name,
-              y: [sortedTasks[i].minTotalTime,
-                sortedTasks[i].totalQuartiles[0],
-                sortedTasks[i].totalQuartiles[1],
-                sortedTasks[i].totalQuartiles[2],
-                sortedTasks[i].maxTotalTime]
-            }
-          )
-        }
-      } else if (this.timingOption === 'runTimes') {
-        for (let i = startingNumber; i < endingNumber; i++) {
-          boxPlot[0].data.push(
-            {
-              x: sortedTasks[i].name,
-              y: [sortedTasks[i].minRunTime,
-                sortedTasks[i].runQuartiles[0],
-                sortedTasks[i].runQuartiles[1],
-                sortedTasks[i].runQuartiles[2],
-                sortedTasks[i].maxRunTime]
-            }
-          )
-        }
-      } else if (this.timingOption === 'queueTimes') {
-        for (let i = startingNumber; i < endingNumber; i++) {
-          boxPlot[0].data.push(
-            {
-              x: sortedTasks[i].name,
-              y: [sortedTasks[i].minQueueTime,
-                sortedTasks[i].queueQuartiles[0],
-                sortedTasks[i].queueQuartiles[1],
-                sortedTasks[i].queueQuartiles[2],
-                sortedTasks[i].maxQueueTime]
-            }
-          )
-        }
-      }
-      return boxPlot
-    },
-
-    chartOptions () {
-      const chartOptions = {
-        chart: {
-          id: this.workflowName,
-          animations: {
-            enabled: true,
-            easing: 'easeinout',
-            speed: 300,
-            animateGradually: {
-              enabled: true,
-              delay: 150
-            },
-            dynamicAnimation: {
-              enabled: true,
-              speed: 350
-            }
-          }
-        },
-        title: {
-          text: this.workflowName
-        },
-        plotOptions: {
-          bar: {
-            horizontal: true
-          },
-          boxPlot: {
-            colors: {
-              upper: '#C4E90C',
-              lower: '#000000'
-            }
-          }
-        }
+      const data = []
+      for (let i = startIndex; i < endIndex; i++) {
+        data.push({
+          x: sortedTasks[i].name,
+          y: [
+            sortedTasks[i][`min${upperFirst(getTimingOption(this.timingOption))}`],
+            sortedTasks[i][`${this.timingOption}Quartiles`][0],
+            sortedTasks[i][`${this.timingOption}Quartiles`][1],
+            sortedTasks[i][`${this.timingOption}Quartiles`][2],
+            sortedTasks[i][`max${upperFirst(getTimingOption(this.timingOption))}`],
+          ],
+        })
       }
       return chartOptions
     }
